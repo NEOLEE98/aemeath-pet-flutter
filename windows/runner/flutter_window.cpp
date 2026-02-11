0e6e1578d2506e8eaa6c4484748354367aa510b0
NEO <lichuyuan13@gmail.com>
2026-02-11 18:13:16 +0800

    feat(windows): set transparent background color for flutter window

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -5,6 +5,10 @@
 #include "flutter/generated_plugin_registrant.h"
 #include "desktop_multi_window/desktop_multi_window_plugin.h"
 
+namespace {
+constexpr COLORREF kTransparentColor = RGB(255, 0, 255);
+}
+
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
 
@@ -25,6 +29,7 @@ bool FlutterWindow::OnCreate() {
   if (!flutter_controller_->engine() || !flutter_controller_->view()) {
     return false;
   }
+  flutter_controller_->view()->SetBackgroundColor(kTransparentColor);
   RegisterPlugins(flutter_controller_->engine());
   DesktopMultiWindowSetWindowCreatedCallback([](void *controller) {
     auto *flutter_view_controller =
